b90bd4552edcc62372dc8754106d5f25f2f3a2c4
nancyk12 <nancyk12@hotmail.com>
2023-06-24 21:11:49 -0400

    edit title button

--- frontend/src/components/ProductCard.js
+++ frontend/src/components/ProductCard.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react'
 
 const ProductCard = (props) => {
 
+    const [editBtn, setEditBtn] = useState(false);
 
     const [editProduct, setEditProduct] = useState({
         id: props.id,
@@ -12,15 +13,55 @@ const ProductCard = (props) => {
         price: props.price
     })
 
+    const onChangeHandler = (e) => {
+        setEditProduct({
+            ...editProduct,
+            [e.target.name]: e.target.value // title: target.value
+        })
+    }
+
+    const saveProduct = () => {
+        props.editProduct(editProduct);
+        setEditBtn(false);
+    }
+
   return (
     <div className='product-card'>
       <h2><u>{props.title}</u></h2>
-      <p>Publisher: {props.publisher}</p>
-      <p>Genre: {props.genre}</p>
-      <p>Price: ${props.price}</p>
+      {
+        editBtn ? 
+        (
+            <div>
+                <lable htmlFor="title">Title: </lable>
+                <input 
+                  type="text"
+                  name="title"
+                  value={editProduct.title}
+                  onChange={onChangeHandler}
+                />
+                <button onClick={saveProduct}>Save Edits! </button>
+                {/* same as below */}
+                {/* <button onClick={() => {
+                    props.editProduct(editProduct)
+                    setEditBtn(false)
+                    }}>Save Edits! </button> */}
+            </div>
+        )
+        : 
+        (
+        <React.Fragment>
+          <p>Publisher: {props.publisher}</p>
+          <p>Genre: {props.genre}</p>
+          <p>Price: ${props.price}</p>
+        </React.Fragment>
+        )}
+
+      <button onClick={() => setEditBtn(!editBtn)}>Edit</button>
+      {/* <button onClick={() => props.editProduct(editProduct)}>Edit!</button> */}
 
       <button onClick={() => props.deleteProduct(props.id)}>Delete!</button>
-      <button onClick={() => props.editProduct(editProduct)}>Edit!</button>
+      
+      
     </div>
   )
 }
